9839ff18951c1313a47ff39ddee40b0a1abfc05d
Priyadec26 <99569858+Priyadec26@users.noreply.github.com>
2022-03-01 23:13:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld
 public static void main(String[] args)
 {
 System.out.println("Git Repository");
-  System.out.println("Push Activated from GITHUB");
+  System.out.println("Push Activated from GIT-HUB");
 }
 }
