e1d8681eb4cd4c0e9d6c3f955feebe39f853fef5
Priyadec26 <99569858+Priyadec26@users.noreply.github.com>
2022-04-27 10:08:44 +0530

    Create AfterMethodinmain.java

---
+++ Static/AfterMethodinmain.java
@@ -0,0 +1,26 @@
+package com.JavaStatic;
+
+public class AfterMethodinmain {
+
+	
+
+	public static void main(String[] args) {
+		
+//		AfterMethodinmain af=new AfterMethodinmain();
+//		
+	        welcome();//Statement
+//	       af.welcometo();
+	}
+
+
+	  static void  welcome() 
+	 {
+		 System.out.println("Welcome to Java");
+	}
+	 void  welcometo() 
+		 {
+		
+			 System.out.println("Welcom to Java");
+		}
+
+}
