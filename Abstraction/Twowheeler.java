4b9224d034d4c2867e7d65d59f7f5829e63083e5
Priyadec26 <99569858+Priyadec26@users.noreply.github.com>
2022-04-27 10:07:29 +0530

    Add files via upload

---
+++ Abstraction/Twowheeler.java
@@ -0,0 +1,16 @@
+package com.Abstraction;
+
+public class Twowheeler 
+{
+	public void nokickstart()
+	{
+		System.out.println("Lakshmi Start aaidu");
+	}
+
+public void Waterallergy() {
+	System.out.println("Guesto will die");
+	
+}
+
+	
+}
