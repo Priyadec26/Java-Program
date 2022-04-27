70fec98f32f4a88311b0464a96e201d559ddf1e9
Priyadec26 <99569858+Priyadec26@users.noreply.github.com>
2022-04-27 10:07:10 +0530

    Create Guesto.java

---
+++ Abstraction/Guesto.java
@@ -0,0 +1,12 @@
+package com.Abstraction;
+
+public class Guesto extends Twowheeler
+{
+	public static void main(String[] args)
+	{
+		Twowheeler guesto = new Guesto();
+		guesto.nokickstart();
+		guesto.Waterallergy();
+	}
+
+}
